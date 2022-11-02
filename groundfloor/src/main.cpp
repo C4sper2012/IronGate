8713a67724b9772c9059ea7b3e4a0d3c2cf2b195
tobinieil04 <tobiniel04@gmail.com>
2022-11-02 12:04:38 +0100

    Added DHT11

--- groundfloor/src/main.cpp
+++ groundfloor/src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
+#include "main.h"
+//#include <../.pio/libdeps/mkrwifi1010/DHT sensor library/DHT.h>
 
 void setup() {
+  Serial.begin(9600);
+  initDHT();
   // put your setup code here, to run once:
 }
 
 void loop() {
+  Serial.print(readTemp());
+  delay(2000);
   // put your main code here, to run repeatedly:
 }
