8a176006c6c797515431b3dbcbdf87b34ef5ceea
Casper <casper.koch.dev@gmail.com>
2022-11-07 11:31:40 +0100

    Added App

---
+++ IronGateApp/IronGateApp/IronGateApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace IronGateApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ IronGateApp/IronGateApp/IronGateApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace IronGateApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
