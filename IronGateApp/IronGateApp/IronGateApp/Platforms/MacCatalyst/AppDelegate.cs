12c5e56fe4ea361f78f990f5063d11a9169085a7
Casper <casper.koch.dev@gmail.com>
2022-11-09 13:50:39 +0100

    Added settingspage and moved mainpage
    Scoped Namespaces

--- IronGateApp/IronGateApp/IronGateApp/Platforms/MacCatalyst/AppDelegate.cs
+++ IronGateApp/IronGateApp/IronGateApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -1,9 +1,10 @@
 ﻿using Foundation;
 
-namespace IronGateApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
+namespace IronGateApp
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
 }
