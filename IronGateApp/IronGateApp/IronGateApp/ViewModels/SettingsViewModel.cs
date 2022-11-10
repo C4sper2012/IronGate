3bf591c42c75c6a20ba1cb83d6ef7aa4f0011ed5
Casper <casper.koch.dev@gmail.com>
2022-11-10 09:45:30 +0100

    Added settingsviewmodel and its properties

--- IronGateApp/IronGateApp/IronGateApp/ViewModels/SettingsViewModel.cs
+++ IronGateApp/IronGateApp/IronGateApp/ViewModels/SettingsViewModel.cs
@@ -1,9 +1,43 @@
-﻿using IronGateApp.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using IronGateApp.Models;
 
 namespace IronGateApp.ViewModels
 {
-    public class SettingsViewModel : BaseViewModel
+    public partial class SettingsViewModel : BaseViewModel
     {
         public Setting Setting { get; } = new();
+
+        #region Sensor detection
+
+        [ObservableProperty]
+        bool _shouldNotifyWaterLevel;
+
+        [ObservableProperty]
+        bool _shouldNotifyMotionDetected;
+
+        #endregion
+
+        #region Floor Climates
+
+        [ObservableProperty]
+        bool _shouldSendFirstFloorClimate;
+
+        [ObservableProperty]
+        bool _shouldSendGroundFloorClimate;
+
+        [ObservableProperty]
+        bool _shouldSendBasementClimate;
+
+        #endregion
+
+        #region Other options
+
+        [ObservableProperty]
+        bool _shouldCallPoliceOnMotionDetected;
+
+        [ObservableProperty]
+        bool _shouldLogMotionEntriesInApp;
+
+        #endregion
     }
 }
