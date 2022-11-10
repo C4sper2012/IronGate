93ed2c3b5a606f117e9084b9f769f112e7ecd8d1
Casper <casper.koch.dev@gmail.com>
2022-11-10 09:12:52 +0100

    Renamed model settings => setting SettingsViewModel inherit from BaseViewModels

--- IronGateApp/IronGateApp/IronGateApp/ViewModels/SettingsViewModel.cs
+++ IronGateApp/IronGateApp/IronGateApp/ViewModels/SettingsViewModel.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using IronGateApp.Models;
 
 namespace IronGateApp.ViewModels
 {
-    public class SettingsViewModel
+    public class SettingsViewModel : BaseViewModel
     {
-
+        public Setting Setting { get; } = new();
     }
 }
