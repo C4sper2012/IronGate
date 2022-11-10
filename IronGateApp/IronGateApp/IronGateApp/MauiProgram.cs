6ad846fa9e582ab9bf61151fde6950502b748aec
tobinieil04 <tobiniel04@gmail.com>
2022-11-10 13:36:55 +0100

    Fix merge hell

--- IronGateApp/IronGateApp/IronGateApp/MauiProgram.cs
+++ IronGateApp/IronGateApp/IronGateApp/MauiProgram.cs
@@ -16,13 +16,13 @@ namespace IronGateApp
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
-            builder.Services.AddSingleton<SettingsViewModel>();
-            builder.Services.AddSingleton<SettingsPage>( );
-
             builder.Services.AddSingleton<ClimateDetailsPage>();
             builder.Services.AddSingleton<ClimatePage>();
             builder.Services.AddSingleton<ClimateViewModel>();
             builder.Services.AddTransient<ClimateDetailsViewModel>();
+
+            builder.Services.AddSingleton<SettingsViewModel>();
+            builder.Services.AddSingleton<SettingsPage>( );
 
             return builder.Build();
         }
