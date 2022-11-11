bb8cf75b7be134e4cc990ae66867667882f8cd41
Nicklas Markvart Nielsen <nicklas.m.nielsen@live.com>
2022-11-11 12:55:15 +0100

    Addded charts

--- IronGateApp/IronGateApp/IronGateApp/MauiProgram.cs
+++ IronGateApp/IronGateApp/IronGateApp/MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using IronGateApp.DatabaseContext;
 using IronGateApp.ViewModels;
 using IronGateApp.Views;
+using Syncfusion.Maui.Core.Hosting;
 
 namespace IronGateApp
 {
@@ -11,22 +12,27 @@ namespace IronGateApp
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .ConfigureSyncfusionCore()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            #region Scopes
             builder.Services.AddSingleton<ClimateDetailsPage>();
             builder.Services.AddSingleton<ClimatePage>();
             builder.Services.AddSingleton<ClimateViewModel>();
             builder.Services.AddTransient<ClimateDetailsViewModel>();
 
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<MainPageViewModel>();
+
             builder.Services.AddSingleton<SettingsViewModel>();
             builder.Services.AddSingleton<SettingsPage>( );
 
             builder.Services.AddTransient<IronGateContext>();
-
+            #endregion
 
             return builder.Build();
         }
