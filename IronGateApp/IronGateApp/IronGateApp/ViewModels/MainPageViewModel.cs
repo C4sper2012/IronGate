6b6fe4c000c301d8faf6cb621aef6a64035edbce
Nicklas M. Nielsen <1n2n3n4n5n@hotmail.dk>
2022-11-12 14:49:30 +0100

    Somthing i dont know yet

--- IronGateApp/IronGateApp/IronGateApp/ViewModels/MainPageViewModel.cs
+++ IronGateApp/IronGateApp/IronGateApp/ViewModels/MainPageViewModel.cs
@@ -1,18 +1,46 @@
-﻿using IronGateApp.Models;
+﻿using CommunityToolkit.Mvvm.Input;
+using IronGateApp.Models;
+using IronGateApp.Services;
+using System.Collections.ObjectModel;
 
-namespace IronGateApp.ViewModels
+namespace IronGateApp.ViewModels;
+public partial class MainPageViewModel : BaseViewModel
 {
-    public class MainPageViewModel
+    public ObservableCollection<RoomDHT11> Data { get; set; } = new();
+
+    private readonly ClimateService _climateService;
+    public MainPageViewModel(ClimateService climateService)
     {
-        public List<RoomDHT11> Data { get; set; }
-        public MainPageViewModel()
+        _climateService = climateService;
+        Task<ObservableCollection<RoomDHT11>> task = GetChartData();
+        Data = task.Result;
+    }
+
+    //[RelayCommand]
+    private async Task<ObservableCollection<RoomDHT11>> GetChartData()
+    {
+        ObservableCollection<RoomDHT11> tmpData = new();
+        Climate climateBasement = await _climateService.GetBasementClimateAsync();
+        tmpData.Add(new()
         {
-            Data = new List<RoomDHT11>()
-            {
-                new RoomDHT11 { Floor = "Basement", Temperature = 23, Humidity = 46 },
-                new() { Floor = "Ground floor", Temperature = 24, Humidity = 51 },
-                new() { Floor = "First floor", Temperature = 23, Humidity = 48 }
-            };
-        }
+            Floor = "Basement",
+            Temperature = Int32.Parse(climateBasement.Channel.BasementTemp)
+        });
+
+        Climate climateGroundFloor = await _climateService.GetGroundFloorClimateAsync();
+        tmpData.Add(new()
+        {
+            Floor = "Ground floor",
+            Temperature = Int32.Parse(climateGroundFloor.Channel.GroundFloorTemp)
+        });
+
+        Climate climateFirstFloor = await _climateService.GetFirstFloorClimateAsync();
+        tmpData.Add(new()
+        {
+            Floor = "First floor",
+            Temperature = Int32.Parse(climateFirstFloor.Channel.FirstFloorTemp)
+        });
+
+        return tmpData;
     }
 }
