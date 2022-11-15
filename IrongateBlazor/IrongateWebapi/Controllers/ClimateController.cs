900d89681b141dc3816a57c94e20575dbff1f4f0
Casper <casper.koch.dev@gmail.com>
2022-11-15 22:09:03 +0100

    Added api

---
+++ IrongateBlazor/IrongateWebapi/Controllers/ClimateController.cs
@@ -0,0 +1,24 @@
+﻿using Irongate.Repository.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Irongate.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClimateController : ControllerBase
+    {
+        [Route("{id:int}")]
+        [HttpGet]
+        public Climate GetClimateById(int id)
+        {
+            return new Climate();
+        }
+        
+        [Route("{dateFrom:datetime}")]
+        [HttpGet]
+        public IEnumerable<Climate> GetClimateById(DateTime dateFrom, DateTime dateTo)
+        {
+            return new List<Climate>(){new(){ Humidity = 25, Temperature = 25, ID = 1, TimeStamp = DateTime.Now}};
+        }
+    }
+}
---
+++ IrongateBlazor/IrongateWebapi/Controllers/SensorController.cs
@@ -0,0 +1,17 @@
+﻿using Irongate.Repository.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Irongate.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SensorController : ControllerBase
+    {
+        [Route("{id:int}")]
+        [HttpGet]
+        public MotionSensor GetClimateById(int id)
+        {
+            return new MotionSensor();
+        }
+    }
+}
