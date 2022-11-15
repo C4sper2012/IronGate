10ddc5627ff4834444aef239339e79e40c443e11
Casper <casper.koch.dev@gmail.com>
2022-11-15 12:58:09 +0100

    Added sql and access

--- IrongateBlazor/Irongate.Repository/Domain/Models/Climate.cs
+++ IrongateBlazor/Irongate.Repository/Domain/Models/Climate.cs
@@ -9,7 +9,7 @@ namespace Irongate.Repository.Domain.Models
 	public class Climate
 	{
 		public int ID { get; set; }
-		public DateTime CreatedAt { get; set; }
+		public DateTime TimeStamp { get; set; }
 		public int Temperature { get; set; }
 		public int Humidity { get; set; }
 	}
