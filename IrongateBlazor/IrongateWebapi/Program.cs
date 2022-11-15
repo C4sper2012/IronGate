06bbcce7eeb155a1a4fa96ebd36c7e44a5ec67a2
Casper <casper.koch.dev@gmail.com>
2022-11-15 15:07:33 +0100

    Added models and database

--- IrongateBlazor/IrongateWebapi/Program.cs
+++ IrongateBlazor/IrongateWebapi/Program.cs
@@ -1,3 +1,6 @@
+using Irongate.Repository.Domain.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<IrongateContext>();
 
 var app = builder.Build();
+using IServiceScope serviceScope = app.Services.CreateScope();
+// serviceScope.ServiceProvider.GetService<IrongateContext>().Database.EnsureCreated();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
