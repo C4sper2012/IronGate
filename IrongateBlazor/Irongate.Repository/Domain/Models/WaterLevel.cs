77de68fab88d3bb0a4bf70af80b9f9cd9187b79b
Casper <casper.koch.dev@gmail.com>
2022-11-15 15:11:41 +0100

    Added models

---
+++ IrongateBlazor/Irongate.Repository/Domain/Models/WaterLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Irongate.Repository.Domain.Models
+{
+    public class WaterLevel
+    {
+        public int ID { get; set; }
+        public int Value { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
