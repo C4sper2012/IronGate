3c72d5ed9b5122a2406d376387e72f886aa474a2
tobinieil04 <tobiniel04@gmail.com>
2022-11-16 13:22:12 +0100

    Added Auth0 Authentication.

---
+++ IrongateBlazor/Irongate.Blazor/Pages/Login/Login.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Irongate.Blazor.Pages.Login;
+
+public class Login : PageModel
+{
+    public async Task OnGet(string returnUrl = "/")
+    {
+        var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+            .WithRedirectUri(returnUrl)
+            .Build();
+        await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    }
+}
