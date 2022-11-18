2d0cc9d35bce7ec668df0413bfbe2dddd1c05c82
Casper <casper.koch.dev@gmail.com>
2022-11-18 12:08:00 +0100

    Added service for log

---
+++ IrongateBlazor/Irongate.Service/Models/Log.cs
@@ -0,0 +1,9 @@
+﻿namespace Irongate.Service.Models
+{
+    public class Log
+    {
+        public int ID { get; set; }
+        public string Message { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
