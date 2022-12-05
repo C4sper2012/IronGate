1d069f01588720723edfd30a1a495800440c9a79
Casper <casper.koch.dev@gmail.com>
2022-12-05 22:37:04 +0100

    Fixed bugs

--- IrongateBlazor/Irongate.Service/Services/LogService.cs
+++ IrongateBlazor/Irongate.Service/Services/LogService.cs
@@ -19,14 +19,21 @@ namespace Irongate.Service.Services
         
         public async Task CreateAsync(Log log)
         {
-            var client = new RestClient($"http://{AppConstants.DatabaseAddress}/Log/Create");
-            var request = new RestRequest();
-            request.Method = Method.Post;
-            request.AddHeader("content-type", "application/json");
-            request.AddHeader("authorization",
-                $"Bearer {await _httpContextAccessor.HttpContext.GetTokenAsync("access_token")}");
-            request.AddJsonBody(JsonConvert.SerializeObject(log));
-            RestResponse response = client.Execute(request);
+            try
+            {
+                var client = new RestClient($"http://{AppConstants.DatabaseAddress}/Log/Create");
+                var request = new RestRequest();
+                request.Method = Method.Post;
+                request.AddHeader("content-type", "application/json");
+                request.AddHeader("authorization",
+                    $"Bearer {await _httpContextAccessor.HttpContext.GetTokenAsync("access_token")}");
+                request.AddJsonBody(JsonConvert.SerializeObject(log));
+                RestResponse response = client.Execute(request);
+            }
+            catch (Exception e)
+            {
+                
+            }
         }
         
         public async Task DeleteAsync()
@@ -42,13 +49,20 @@ namespace Irongate.Service.Services
 
         public async Task<List<Log>> GetLogs()
         {
-            var client = new RestClient($"http://{AppConstants.DatabaseAddress}/Log/all");
-            var request = new RestRequest();
-            request.Method = Method.Get;
-            request.AddHeader("content-type", "application/json");
-            request.AddHeader("authorization", $"Bearer {await _httpContextAccessor.HttpContext.GetTokenAsync("access_token")}");
-            RestResponse response = await client.ExecuteAsync(request);
-            return JsonConvert.DeserializeObject<List<Log>>(response.Content);
+            try
+            {
+                var client = new RestClient($"http://{AppConstants.DatabaseAddress}/Log/all");
+                var request = new RestRequest();
+                request.Method = Method.Get;
+                request.AddHeader("content-type", "application/json");
+                request.AddHeader("authorization", $"Bearer {await _httpContextAccessor.HttpContext.GetTokenAsync("access_token")}");
+                RestResponse response = await client.ExecuteAsync(request);
+                return JsonConvert.DeserializeObject<List<Log>>(response.Content);
+            }
+            catch (Exception e)
+            {
+                return new List<Log>();
+            }
         }
     }
 }
